19280d21ee98828e65b515536c9a2ba37c91f0a2
kstiopin <kstiopin@brightgrove.com>
2019-03-12 14:12:22 +0200

    Collect reports for first guild on server for single encounter

--- pages/wpGuild.js
+++ pages/wpGuild.js
@@ -1,8 +1,10 @@
 module.exports = {
   elements: {},
   commands: [{
-    load: function(guildName) {
-      return this.api.url('https://www.wowprogress.com/guild/eu/гордунни/' + guildName);
+    loadGuild: function(guildName) {
+      return this
+          .api.url('https://www.wowprogress.com/guild/eu/гордунни/' + guildName)
+          .waitForElementPresent('table.rating', 1000);
     }
   }]
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,42 @@
-let wpGuildPage;
+const guilds = require('../data.json');
+const guildResults = {};
 
 module.exports = {
-  before: function (client) {
-    wpGuildPage = client.page.wpGuild();
+  before: function () {
     startTime = Date.now();
   },
-  after: function (client) {
-    console.log('total time ' + this.tags[0] + ' ' + (Date.now() - startTime) / defaultDelay + 'sec');
-    client.end();
+  after: function () {
+    console.log(`total time ${Date.now() - startTime}sec`, guildResults);
   },
-  'Load test guild': function() {
-    wpGuildPage.load('Ингениум');
+  'Loading guilds': function(client) {
+    guilds.slice(0, 1).forEach(guild => {
+      const guildName = guild.name;
+      guildResults[guildName] = {};
+      const bossId = 589;
+      guildResults[guildName][bossId] = {};
+      client.page.wpGuild().loadGuild(guildName);
+      client.executeAsync(function(guildName, bossId, done) {
+        window._asyncResult = undefined;
+        new Promise(function(resolve) {
+          fetch(`https://www.wowprogress.com/guild/eu/гордунни/${guildName}?boss_kills=${bossId}`).then(response => {
+            resolve(response.text());
+          });
+        }).then(response => {
+          window._asyncResult = response;
+          done(window._asyncResult);
+        });
+      }, [guildName, bossId], function(resp) {
+        console.log(`${guild.name} loaded`);
+        const dataRows = resp.value.split(`data-aid="${bossId}_`).slice(1);
+        guildResults[guildName][bossId] = {};
+        dataRows.forEach(dataRow => {
+          const data = dataRow.split('" class="raidcomp_link innerLink"');
+          const reportKey = data[0];
+          const reportDate = data[1].split('aria-label="')[1].split('" class="hint')[0];
+          guildResults[guildName][bossId][reportDate] = reportKey;
+        });
+      });
+      client.end();
+    });
   }
 };
