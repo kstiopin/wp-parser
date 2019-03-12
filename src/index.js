b0a22a63c129937a6662b1d6fd5da3f745c6dd45
kstiopin <kstiopin@brightgrove.com>
2019-03-12 14:50:46 +0200

    Collect raid roasters for every kill of selected encounter

--- src/index.js
+++ src/index.js
@@ -26,14 +26,36 @@ module.exports = {
           done(window._asyncResult);
         });
       }, [guildName, bossId], function(resp) {
-        console.log(`${guild.name} loaded`);
-        const dataRows = resp.value.split(`data-aid="${bossId}_`).slice(1);
+        console.log(`--> Guild ${guild.name} loaded`);
+        const dataRows = resp.value.split('data-aid="').slice(1);
         guildResults[guildName][bossId] = {};
         dataRows.forEach(dataRow => {
           const data = dataRow.split('" class="raidcomp_link innerLink"');
           const reportKey = data[0];
           const reportDate = data[1].split('aria-label="')[1].split('" class="hint')[0];
-          guildResults[guildName][bossId][reportDate] = reportKey;
+          guildResults[guildName][bossId][reportDate] = [];
+
+          client.executeAsync(function(guildName, reportKey, done) {
+            window._asyncResult = undefined;
+            new Promise(function(resolve) {
+              fetch(`https://www.wowprogress.com/guild/eu/гордунни/${guildName}?raidcomp=${reportKey}`).then(response => {
+                resolve(response.text());
+              });
+            }).then(response => {
+              window._asyncResult = response;
+              done(window._asyncResult);
+            });
+          }, [guildName, reportKey], function(resp) {
+            console.log(`---> Data for report ${reportKey} loaded`);
+            const raidSetup = resp.value.split('<tr>').slice(2);
+            raidSetup.forEach(characterDataRow => {
+              const characterData = characterDataRow.split('<td>');
+              let characterInfo = characterData[1].replace('</td>', ' ');
+              const characterExtraInfo = characterData[2].replace('<a aria-label="', '').replace('</a></td>', '').split('"');
+              characterInfo += characterExtraInfo[0] + characterExtraInfo[characterExtraInfo.length - 1].replace('>', ' ');
+              guildResults[guildName][bossId][reportDate].push(characterInfo);
+            });
+          });
         });
       });
       client.end();
